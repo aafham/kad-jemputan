15a0fabc9b642b74e8197a8283528e6466acc40e
Afham Azizan <aafham21@gmail.com>
2026-02-05 11:50:54 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ btn.onclick = () => {
   btn.style.opacity = "0";
   btn.style.pointerEvents = "none";
 };
+
+const eventDate = new Date("2026-08-20T11:00:00").getTime();
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const diff = eventDate - now;
+
+  document.getElementById("days").innerText =
+    Math.floor(diff / (1000 * 60 * 60 * 24));
+  document.getElementById("hours").innerText =
+    Math.floor((diff / (1000 * 60 * 60)) % 24);
+  document.getElementById("minutes").innerText =
+    Math.floor((diff / (1000 * 60)) % 60);
+  document.getElementById("seconds").innerText =
+    Math.floor((diff / 1000) % 60);
+}, 1000);
+
+const fades = document.querySelectorAll(".fade");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+fades.forEach(el => observer.observe(el));
