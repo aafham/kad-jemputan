9e2df294eee975b97246b9c79ac7791e1bacec9c
Afham Azizan <aafham21@gmail.com>
2026-02-05 10:09:55 +0800

    Change class name from 'open' to 'opened'

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-document.getElementById("btn").onclick = () => {
-  document.body.classList.add("open");
-};
 const btn = document.getElementById("btn");
 
 btn.onclick = () => {
-  document.body.classList.add("open");
+  document.body.classList.add("opened");
   btn.style.opacity = "0";
   btn.style.pointerEvents = "none";
 };
